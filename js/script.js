809407a3279e97e799fe23468f5d471b43bf26bf
Garrett Stewart <satanthesith@gmail.com>
2015-12-13 07:15:45 -0800

    Cleaned up html, fixed individual card flipping

--- js/script.js
+++ js/script.js
@@ -1,26 +1,5 @@
 $(document).ready(function(){
-	
-	$("#btn_small").click(function(){
-		$(".flip-container,.front,.back,.front img,.back img")
-		.width(200).height(150);
-	})
-	
-	$("#btn_med").click(function(){
-		$(".flip-container,.front,.back,.front img,.back img")
-		.width(400).height(300);
-	})
-	
-	$("#btn_large").click(function(){
-		$(".flip-container,.front,.back,.front img,.back img")
-		.width(600).height(450);
-	})
-	
-	/* $(".front").click(function(){
-		
-		var newText= $("textarea").val();
-		console.log(newText);
-		$(".cardText p").text(newText);
-	}) */
+
 	$("#submit").click(function(){
 		var frontText = $("#frontText").val();
 		var backText = $("#backText").val();
@@ -28,30 +7,14 @@ $(document).ready(function(){
 		console.log(backText);
 		
 		$(".createF .cardText p").text(frontText);
-		$(".createB .cardText p").text(backText);
+		$(".createB .cardText p").text(backText)
 	})
-	$(".flipButton").click(function(){
-	
-		$(".active").addClass("oldActive");
-		$(".active").removeClass("active");
-		
-		$(".hidden").addClass("active");
-		$(".hidden").removeClass("hidden");
-		
-		$(".oldActive").addClass("hidden");
-		$(".oldActive").removeClass("oldActive");
-	})
-	
 	
 	function IndexCard(frontText, backText){
 		this.cFrontText = frontText;
 		this.cBackText = backText;
 	}
-	/* 
-	card1 = new IndexCard(1,"test","test 2"); */
-/* 	card = "hello";
-	 */
-	 
+
 	savedCards = [];
 	
 	$("#save").click(function(){
@@ -60,28 +23,35 @@ $(document).ready(function(){
 		var num = savedCards.length;
 		
 		savedCards[num] = new IndexCard(front,back);
-		$(".savedCards").append('<div class="indexWrap"><div class="front active">\
-			<img src="img/arrow.png" class="flipButton"></img>\
+		$(".savedCards").append('<div class="indexWrap"><img src="img/arrow.png" class="flipButton"></img>\
+		<div class="front active">\
 			<div class="cardText">\
 				<p>'+front+'</p>\
 			</div>\
 		</div>\
 		<div class="back hidden">\
-			<img src="img/arrow.png" class="flipButton"></img>\
 			<div class="cardText">\
 				<p>'+back+'</p>\
 			</div>\
 		</div>\
-	</div>\
 	</div>');
 	})
 	
-	
-	
-	$(".flipper")
-	$(".front")
-	$(".back")
-	 
-	
+	$(document).on('click','.flipButton',function(){
+		
+		//alert(1);
+		var fcActive = $(this).siblings(".active");
+		var fcHidden = $(this).siblings(".hidden");
+		
+		$(fcActive).addClass("oldActive");
+		$(fcActive).removeClass("active");
+		
+		$(fcHidden).addClass("active");
+		$(fcHidden).removeClass("hidden");
+		
+		$(".oldActive").addClass("hidden");
+		$(".oldActive").removeClass("oldActive");
+		
+	})	
 	
 })
